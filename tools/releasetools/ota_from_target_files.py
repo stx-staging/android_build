3d1e3d5cd45aa8ea37551bd2d33171758e2fa30e
LuK1337 <priv.luk@gmail.com>
2018-12-05 20:51:49 +0100

    build: Support system-as-root A-only backuptool
    Change-Id: Ia24ae288a48721f724bfebadfe023330c4932633

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -827,9 +827,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
 
   if OPTIONS.backuptool:
+    is_system_as_root = script.fstab["/system"].mount_point == "/"
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/system"
     script.Mount("/system")
-    script.RunBackup("backup")
+    script.RunBackup("backup", "/system/system" if is_system_as_root else "/system")
     script.Unmount("/system")
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/"
 
   # All other partitions as well as the data wipe use 10% of the progress, and
   # the update of the system partition takes the remaining progress.
@@ -864,11 +869,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
+    is_system_as_root = script.fstab["/system"].mount_point == "/"
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/system"
     if OPTIONS.block_based:
       script.Mount("/system")
-    script.RunBackup("restore")
+    script.RunBackup("restore", "/system/system" if is_system_as_root else "/system")
     if OPTIONS.block_based:
       script.Unmount("/system")
+    if is_system_as_root:
+      script.fstab["/system"].mount_point = "/"
 
   script.WriteRawImage("/boot", "boot.img")
 
