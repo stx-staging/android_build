6401ff7f0ecb8a4ccb9240d61c3cc6a6098163ac
Jake Weinstein <jake@aospa.co>
2019-11-04 22:17:43 +0100

    [SQUASH] build: enable support for vendor_overlay in makefile
    This is a combination of 3 commits.
    This is the 1st commit message:
    
    build: make: allow modules to be installed to vendor_overlay
    
    vendor_overlay allows us to make changes to vendor image
    from product for system-only builds.
    
    Change-Id: Ief50c288621a9f4ff16c67e764d90c21caca9d6d
    
    This is the commit message #2:
    
    [SQUASHME] build: follow-up fixes to vendor_overlay
    
    - Define placeholder vendor overlay path which fixes random build errors.
    - Correct typo in clean rule
    - Fix spacing
    
    Change-Id: Id64dc253c962a8575be5ec6a00dda52d6dc47dd1
    
    This is the commit message #3:
    
    [SQUASHME] build: more vendor_overlay fixes
    
    - Add LOCAL_VENDOR_OVERLAY_MODULE to clear_vars
    - ENABLE LOCAL_USE_VNDK on LOCAL_VENDOR_OVERLAY_MODULE
    
    Change-Id: Icd64270bcf2960d04c703c625018295f90e28527

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -83,6 +83,8 @@ else ifneq ($(filter $(TARGET_OUT_PRODUCT)/%,$(_path)),)
 LOCAL_PRODUCT_MODULE := true
 else ifneq ($(filter $(TARGET_OUT_SYSTEM_EXT)/%,$(_path)),)
 LOCAL_SYSTEM_EXT_MODULE := true
+else ifneq ($(filter $(TARGET_OUT_VENDOR_OVERLAY)/%,$(_path)),)
+LOCAL_VENDOR_OVERLAY_MODULE := true
 endif
 _path :=
 
@@ -106,7 +108,8 @@ non_system_module := $(filter true, \
    $(LOCAL_PRODUCT_MODULE) \
    $(LOCAL_SYSTEM_EXT_MODULE) \
    $(LOCAL_VENDOR_MODULE) \
-   $(LOCAL_PROPRIETARY_MODULE))
+   $(LOCAL_PROPRIETARY_MODULE) \
+   $(LOCAL_VENDOR_OVERLAY_MODULE))
 
 include $(BUILD_SYSTEM)/local_vndk.mk
 include $(BUILD_SYSTEM)/local_systemsdk.mk
@@ -221,6 +224,9 @@ else ifeq (true,$(strip $(LOCAL_SYSTEM_EXT_MODULE)))
   partition_tag := _SYSTEM_EXT
 else ifeq (NATIVE_TESTS,$(LOCAL_MODULE_CLASS))
   partition_tag := _DATA
+else ifeq (true,$(strip $(LOCAL_VENDOR_OVERLAY_MODULE)))
+  partition_tag := _VENDOR_OVERLAY
+
 else
   # The definition of should-install-to-system will be different depending
   # on which goal (e.g., sdk or just droid) is being built.
